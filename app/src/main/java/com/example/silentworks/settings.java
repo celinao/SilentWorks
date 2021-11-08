a7d3b38eec45d7290d29b2cb8a8bb77f359020de
Celina Ough <ough@wisc.edu>
2021-11-07 23:20:18 -0600

    Basic Calendar layout

--- app/src/main/java/com/example/silentworks/settings.java
+++ app/src/main/java/com/example/silentworks/settings.java
@@ -2,12 +2,40 @@ package com.example.silentworks;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.NumberPicker;
 
 public class settings extends AppCompatActivity {
+
+
+    private NumberPicker hourPicker;
+    private NumberPicker minPicker;
+    private String[] hourVals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        hourPicker = findViewById(R.id.TimerHour);
+        hourPicker.setMaxValue(5);
+        hourPicker.setMinValue(0);
+        hourVals = new String[] {"A", "1", "2", "3", "4", "5"};
+        hourPicker.setDisplayedValues(hourVals);
+
+        hourPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int valuePicker1 = hourPicker.getValue();
+                Log.d("picker value", hourVals[valuePicker1]);
+            }
+        });
+
+        minPicker = findViewById(R.id.TimerMin);
+        minPicker.setMinValue(0);
+        minPicker.setMaxValue(59);
+
         setContentView(R.layout.activity_settings);
     }
+
 }
