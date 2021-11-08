2af4ca7abb51c49000d6aacdecd88e2a856b83d8
Celina Ough <ough@wisc.edu>
2021-11-08 16:41:52 -0600

    Set first page to Login-Page

--- app/src/main/java/com/example/silentworks/MainPage.java
+++ app/src/main/java/com/example/silentworks/MainPage.java
@@ -9,12 +9,14 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainPage extends OptionsMenu {
+    // remove Options Menu and replace with AppCompat once the log-in features work since there
+    // shouldn't be a menu on this page.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_calendar);
     }
 
 
