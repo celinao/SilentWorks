8e782fb56ff413f48cbd53e3c665e5142b4d2353
Heqiao Zhu <hzhu254@wisc.edu>
2021-11-25 14:23:36 -0600

    Notifications can be seen on NotificationPage

--- app/src/main/java/com/example/silentworks/NotificationListener.java
+++ app/src/main/java/com/example/silentworks/NotificationListener.java
@@ -1,15 +1,19 @@
 package com.example.silentworks;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
 
+
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class NotificationListener extends NotificationListenerService {
 
-    private String TAG = "NotificationListener";
+    private String TAG = "SuperUnique";
     @Override
     public IBinder onBind(Intent intent) {
         return super.onBind(intent);
@@ -29,11 +33,11 @@ public class NotificationListener extends NotificationListenerService {
         super.onNotificationRemoved(sbn);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onListenerConnected() {
         StatusBarNotification[] sb = getActiveNotifications();
         for (StatusBarNotification d : sb) {
-            Log.d(TAG, "" + d.toString());
             NotificationList.Notifications.add(d);
         }
 //        Intent broadcastIntent = new Intent();
