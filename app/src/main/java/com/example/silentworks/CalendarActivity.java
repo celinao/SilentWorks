572c1677a0d6ad8fca023b785a3e2f655a535d27
Stephen Paoli II <smpaoli@wisc.edu>
2021-12-10 18:39:47 -0600

    CalendarActivity cleanup and changing "Home" list option in menu to "logout"

--- app/src/main/java/com/example/silentworks/CalendarActivity.java
+++ app/src/main/java/com/example/silentworks/CalendarActivity.java
@@ -21,8 +21,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -33,18 +33,18 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
     CalendarView calendarView;
     ArrayList<View> viewList;
     private static final int PERMISSIONS_REQUEST_READ_AND_WRITE_CALENDAR = 12;
-    private static final int PERMISSIONS_REQUEST_WRITE_CALENDAR = 13;
     private long calendarId = 3;
+
     // Projection array. Creating indices for this array instead of doing
-// dynamic lookups improves performance.
-    public static final String[] CALENDAR_PROJECTION = new String[] {
+    // dynamic lookups improves performance.
+    public static final String[] CALENDAR_PROJECTION = new String[]{
             CalendarContract.Calendars._ID,
             CalendarContract.Calendars.ACCOUNT_NAME,
             CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
             CalendarContract.Calendars.OWNER_ACCOUNT
     };
 
-    public static final String[] EVENT_PROJECTION = new String[] {
+    public static final String[] EVENT_PROJECTION = new String[]{
             CalendarContract.Events.TITLE,
             CalendarContract.Events.DESCRIPTION,
             CalendarContract.Events.DTSTART,
@@ -60,7 +60,6 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
     private static final int PROJECTION_DESCRIPTION_NAME_INDEX = 1;
     private static final int PROJECTION_START_INDEX = 2;
     private static final int PROJECTION_END_INDEX = 3;
-    private LinearLayout linearLayout;
     private ArrayList<Event> calendarEvents;
 
     @Override
@@ -69,10 +68,13 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
         setContentView(R.layout.activity_calendar);
         Intent intent = getIntent();
 
+        // Checks if account is already saved from the last login
         GoogleSignInAccount checkAccount = com.google.android.gms.auth.api.signin.GoogleSignIn.getLastSignedInAccount(this);
-        if(checkAccount != null) {
+        if (checkAccount != null) {
+            // Sets account to the last logged in account
             account = checkAccount;
         } else {
+            // Sets account to the account passed in from the previous intent
             account = intent.getParcelableExtra("account");
         }
 
@@ -91,16 +93,17 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
     }
 
     private void displayDayEvents(CalendarView view, int year, int month, int dayOfMonth) {
-        // write code to change the displayed events based on this.
-        Log.v("date", "year: " + String.valueOf(year) + " " + "month: " + String.valueOf(month) + " " + "day: " + String.valueOf(dayOfMonth));
         ArrayList<Event> dayEvents = new ArrayList<Event>();
-        for(int i = 0; i < calendarEvents.size(); i++) {
-            int eventDay = Integer.parseInt(calendarEvents.get(i).getDate().substring(8,10));
-            String eventMonth = calendarEvents.get(i).getDate().substring(4,7);
-            int eventMonthAsInt = 100;
-            int eventYear = Integer.parseInt(calendarEvents.get(i).getDate().substring(24,28));
 
-            switch(eventMonth) {
+        // Loops through all of the events in the calendar, and if the date of the event
+        // matches the date that was clicked on by the user, add it to the dayEvents ArrayList
+        for (int i = 0; i < calendarEvents.size(); i++) {
+            int eventDay = Integer.parseInt(calendarEvents.get(i).getDate().substring(8, 10));
+            String eventMonth = calendarEvents.get(i).getDate().substring(4, 7);
+            int eventMonthAsInt = 100;
+            int eventYear = Integer.parseInt(calendarEvents.get(i).getDate().substring(24, 28));
+
+            switch (eventMonth) {
                 case "Jan":
                     eventMonthAsInt = 0;
                     break;
@@ -139,17 +142,15 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
                     break;
             }
 
-            if(eventDay == dayOfMonth && eventMonthAsInt == month && eventYear == year) {
+            if (eventDay == dayOfMonth && eventMonthAsInt == month && eventYear == year) {
                 dayEvents.add(calendarEvents.get(i));
-                Log.v("EVENT", "year: " + String.valueOf(eventYear) + " " + "month: " + String.valueOf(eventMonthAsInt) + " " + "day: " + String.valueOf(eventDay));
                 setAlarms(calendarEvents.get(i));
             }
         }
         displayCalendarEvents(dayEvents);
     }
 
-    private void setAlarms(Event event){
-        Log.v("Time",  "TIME" + event.getCalendarText());
+    private void setAlarms(Event event) {
         // Call Alarms on Start & End Times
         // Check if event has been turned on/off?
         // Turn on/off based on Settings Page Standard/All/None.
@@ -171,7 +172,7 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
 
     private void searchCalendarTable() {
 
-        // off-line mode getting the events from the storage if any
+        // Off-line mode getting the events from the storage if any
         if (isNetworkAvailable()) {
             LoginStorage loginStorage = new LoginStorage(getApplicationContext());
             String username = loginStorage.getUsername();
@@ -185,43 +186,44 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
                 Manifest.permission.READ_CALENDAR);
         int permissionTwo = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
                 Manifest.permission.WRITE_CALENDAR);
-        if(permission == PackageManager.PERMISSION_DENIED && permissionTwo == PackageManager.PERMISSION_DENIED) {
+
+        // Requesting permission from the user to read/write to/from their calendar if not
+        // already granted
+        if (permission == PackageManager.PERMISSION_DENIED && permissionTwo == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR},
                     PERMISSIONS_REQUEST_READ_AND_WRITE_CALENDAR);
         } else {
-            // Run query
+            // Querying the user's Google calendars to find their personal calendar
             Cursor cur = null;
             ContentResolver cr = getContentResolver();
             Uri uri = CalendarContract.Calendars.CONTENT_URI;
             String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
                     + CalendarContract.Calendars.ACCOUNT_TYPE + " = ? ))";
-            String[] selectionArgs = new String[] {account.getEmail(), "com.google"};
+            String[] selectionArgs = new String[]{account.getEmail(), "com.google"};
             try {
                 // Submit the query and get a Cursor object back
                 cur = cr.query(uri, CALENDAR_PROJECTION, selection, selectionArgs, null);
             } catch (Exception e) {
                 Log.e("Query failed", e.getLocalizedMessage());
             }
-            // Use the cursor to step through the returned records
+            // Use the cursor to step through the returned calendars
             while (cur.moveToNext()) {
                 long calID = 0;
                 String displayName = null;
                 String accountName = null;
                 String ownerName = null;
 
-                // Get the field values
+                // Get the field values of each calendar
                 calID = cur.getLong(PROJECTION_ID_INDEX);
                 displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
                 accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
                 ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
 
-                if(displayName.equals(accountName)){
+                if (displayName.equals(accountName)) {
+                    // Saving the user's personal calendar ID to use later
                     calendarId = calID;
                 }
-
-                Log.v("Calendar values", String.valueOf(calID) + String.valueOf(displayName) +
-                        String.valueOf(accountName) + String.valueOf(ownerName));
             }
 
             // Get new uri to query the events table
@@ -235,12 +237,15 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
             EventsStorage eventsStorage = new EventsStorage(getApplicationContext());
 
             calendarEvents.clear();
-            while(cur.moveToNext()) {
+
+            // Querying all of the events in the user's personal calendar
+            while (cur.moveToNext()) {
                 String title = null;
                 String description = null;
                 long startTime;
                 long endTime;
 
+                // Setting event details
                 title = cur.getString(PROJECTION_TITLE_INDEX);
                 description = cur.getString(PROJECTION_DESCRIPTION_NAME_INDEX);
                 startTime = cur.getLong(PROJECTION_START_INDEX);
@@ -249,14 +254,11 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
                 Date startDate = new Date(startTime);
                 Date endDate = new Date(endTime);
 
-                Log.v("Events", String.valueOf(title) + String.valueOf(description)
-                        + String.valueOf(startTime) + String.valueOf(endTime));
-
-                // Format date for UI
+                // Formatting date for UI
                 SimpleDateFormat formatterHour = new SimpleDateFormat("HH");
                 SimpleDateFormat formatterMin = new SimpleDateFormat("mm");
 
-                // Create an event to be added to the ArrayList
+                // Creating a temporary event to be added to the ArrayList
                 Event tempEvent = new Event(account.getEmail(), startDate.toString(), formatterHour.format(startDate),
                         formatterMin.format(startDate), formatterHour.format(endDate), formatterMin.format(endDate), title, description, "false");
 
@@ -266,34 +268,34 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
                 eventsStorage.deleteEvent(username);
                 String titlePass = title;
                 if (title.contains("'")) {
-                    titlePass = title.replace("'","");
+                    titlePass = title.replace("'", "");
                 }
                 String descriptionPass = description;
                 if (description.contains("'")) {
-                    descriptionPass = description.replace("'","");
+                    descriptionPass = description.replace("'", "");
                 }
                 eventsStorage.saveEvent(username, startDate.toString(), formatterHour.format(startDate),
                         formatterMin.format(startDate), formatterHour.format(endDate), formatterMin.format(endDate), titlePass, descriptionPass, "false");
 
             }
-            //displayCalendarEvents(calendarEvents);
         }
     }
 
     public void displayCalendarEvents(ArrayList<Event> events) {
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
         TextView newView;
-        for(int i = 0; i < viewList.size(); i++) {
+        for (int i = 0; i < viewList.size(); i++) {
             linearLayout.removeView(viewList.get(i));
         }
         viewList.clear();
+
         // Adds each element in the CalendarEvent's array to the display
-        for(int idx = 0; idx < events.size(); idx++) {
+        for (int idx = 0; idx < events.size(); idx++) {
             newView = new TextView(this);
             newView.setText(events.get(idx).getCalendarText());
             newView.setTextSize(20);
 
-            // Switches background color so every other event
+            // Switches background color for every other event
             if (idx % 2 == 0) {
                 newView.setBackgroundResource(R.drawable.calendar_color1);
             } else {
@@ -308,7 +310,7 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             searchCalendarTable();
         }
     }
@@ -325,6 +327,9 @@ public class CalendarActivity extends OptionsMenu implements Serializable {
         beginTime.set(2021, 10, 24, 7, 30);
         Calendar endTime = Calendar.getInstance();
         endTime.set(2021, 10, 24, 8, 30);
+        // This intent automatically takes the user to Google to add an event to their
+        // Calendar. The values that are set on the intent are the default event values
+        // when Google loads
         Intent intent = new Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
